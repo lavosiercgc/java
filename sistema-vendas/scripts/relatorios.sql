760aab2af448aac71841a27e8729cf6cefa9117c
lavosiercgc <83882309+lavosiercgc@users.noreply.github.com>
2021-06-10 11:04:33 -0300

    Add files via upload

---
+++ sistema-vendas/scripts/relatorios.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE  IF NOT EXISTS dbsistemavenda;
+
+USE dbsistemavenda;
+
+SELECT * FROM TABLE tbcliente;
+
+SELECT * FROM TABLE tbfornecedor;
+
+SELECT * FROM TABLE tbproduto;
+
+SELECT * FROM TABLE tbvenda;
+
+SELECT * FROM TABLE tbcompra;
+
+SELECT * FROM TABLE tbitemvenda;
+
+SELECT * FROM TABLE;
