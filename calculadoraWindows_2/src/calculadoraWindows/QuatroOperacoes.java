2f3a151b4a5ebfd6ac7381f48d34e7b217cd1c6b
lavosiercgc <83882309+lavosiercgc@users.noreply.github.com>
2021-06-14 21:51:48 -0300

    Update QuatroOperacoes.java
    atualização do dia 14-06-2021

--- calculadoraWindows_2/src/calculadoraWindows/QuatroOperacoes.java
+++ calculadoraWindows_2/src/calculadoraWindows/QuatroOperacoes.java
@@ -1,35 +1,47 @@
 package calculadoraWindows;
 
 public class QuatroOperacoes {
+
 	public double valor1;
 	public double valor2;
 	public double calculo;
 
 	public double somar(double valor1, double valor2) {
 		calculo = (valor1 + valor2);
-		System.out.println(calculo);
+
 		return calculo;
 
 	}
+
 	public double subtrair(double valor1, double valor2) {
 
 		calculo = (valor1 - valor2);
-		System.out.println(calculo);
+
 		return calculo;
 
 	}
+
 	public double multiplicar(double valor1, double valor2) {
 
 		calculo = (valor1 * valor2);
-		System.out.println(calculo);
+
 		return calculo;
 
 	}
+
 	public double dividir(double valor1, double valor2) {
 
 		calculo = (valor1 / valor2);
-		System.out.println(calculo);
+
 		return calculo;
 
 	}
+
+	public double percent(double valor1, double valor2) {
+		calculo = (valor1 * valor2) / 100;
+
+		return calculo;
+
+	}
+
 }
