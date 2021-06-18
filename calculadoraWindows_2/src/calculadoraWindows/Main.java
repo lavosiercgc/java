7ed609d8b87ea2f6a0cec086a53a7c44137b935d
lavosiercgc <83882309+lavosiercgc@users.noreply.github.com>
2021-06-18 15:36:41 -0300

    trabalhando com ouvidores
    trabalhando com ouvidores

--- calculadoraWindows_2/src/calculadoraWindows/Main.java
+++ calculadoraWindows_2/src/calculadoraWindows/Main.java
@@ -2,6 +2,8 @@ package calculadoraWindows;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Main {
 
@@ -20,5 +22,25 @@ public class Main {
 
 		jframe.setVisible(true);
 
+	}
+}
+
+class MKeyListenerNow extends KeyAdapter{
+	@Override
+	public void keyPressed(KeyEvent event) {
+
+		char ch = event.getKeyChar();
+
+		if (ch == 'a' || ch == 'b' || ch == 'c') {
+
+			System.out.println(event.getKeyChar());
+
+		}
+
+		if (event.getKeyCode() == KeyEvent.VK_HOME) {
+
+			System.out.println("Key codes: " + event.getKeyCode());
+
+		}
 	}
 }
