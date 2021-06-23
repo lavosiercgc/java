e04b9794d921c281668e980426551a467467ef41
lavosiercgc <83882309+lavosiercgc@users.noreply.github.com>
2021-06-22 21:43:07 -0300

    atualizaçao do dia 22-06-2021
    atualizaçao do dia 22-06-2021

--- calculadoraWindows_2/src/calculadoraWindows/QuatroOperacoes.java
+++ calculadoraWindows_2/src/calculadoraWindows/QuatroOperacoes.java
@@ -1,10 +1,31 @@
 package calculadoraWindows;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class QuatroOperacoes {
 
 	public double valor1;
 	public double valor2;
 	public double calculo;
+	public String expressao;
+	
+	public Object calculaExpressao(String expressao) throws ScriptException{
+		
+		String valor = expressao;
+        // create a script engine manager
+        ScriptEngineManager factory = new ScriptEngineManager();
+        // create a JavaScript engine
+        ScriptEngine engine = factory.getEngineByName("JavaScript");
+        // evaluate JavaScript code from String
+        Object obj = engine.eval(valor);
+        String fim = String.valueOf(obj);
+        //System.out.println (fim); // imprime "1.0"
+        //System.out.println (obj.getClass()); // imprime "java.lang.Double"
+        
+		return fim ;
+	}
 
 	public double somar(double valor1, double valor2) {
 		calculo = (valor1 + valor2);
